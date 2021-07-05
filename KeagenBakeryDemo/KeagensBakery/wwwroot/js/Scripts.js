39fa80df8e9967d5726e599f95fcaab46874901d
Brandon Toppel <toppelbman@gmail.com>
2021-07-05 13:28:54 -0400

    Changes to CSS
    Changed how the CSS effects the products on their own pages. Combined them into one class that way each page looks consistent.

--- KeagenBakeryDemo/KeagensBakery/wwwroot/js/Scripts.js
+++ KeagenBakeryDemo/KeagensBakery/wwwroot/js/Scripts.js
@@ -1,6 +1,5 @@
 ﻿var slideIndex = 0;
 showSlides();
-
 //slide show for homepage
 function showSlides() {
     var i;
@@ -11,21 +10,25 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
 //function for dropdown product list
 function OptionSelection() {
-    var obj = document.getElementsById("BrownieCookies");
-    var SingleImg = document.getElementById("SingleCookie");
-    var PackageImg = document.getElementById("PackageCookie");
-    if (obj.options[obj.selectedIndex].Text == "Single Cookie $1.29") {
-        SingleImg.style.visibility = 'visable';
-    }
-    if (obj.options[obj.selectedIndex].Text == "6 Cookies $5.99") {
-        SingleImg.style.visibility = 'none';
-        PackageImg.style.visibility = 'visable';
-    }
+    console.log(25);
+    //var obj = document.getElementsById("BrownieCookies");
+    //var SingleImg = document.getElementById("SingleCookie");
+    //var PackageImg = document.getElementById("PackageCookie");
+    //SingleImg.style.display = 'none';
+    //PackageImg.style.display = 'inline';
+    //if (obj.options[obj.selectedIndex].Text == "Single Cookie $1.29") {
+    //    PackageImg.style.display = 'none';
+    //    SingleImg.style.display = 'inline';
+    //}
+    //if (obj.options[obj.selectedIndex].Text == "6 Cookies $5.99") {
+    //    SingleImg.style.display = 'none';
+    //    PackageImg.style.display = 'inline';
+    //}
 }
 
 
