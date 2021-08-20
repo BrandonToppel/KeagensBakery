7759b8aa048d4748a79fb9808ca1e48668543cac
Brandon Toppel <toppelbman@gmail.com>
2021-08-20 17:04:24 -0400

    Connected Web API and Client program
    Connected the Server web api to the client web application. For now it is only getting user and displaying users in a location users cannot get to. Web API is no longer throwing an error 500 messaage.

---
+++ KeagenBakeryDemo/KeagensBakery/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using KeagensBakery.Models;
+using KeagensBakery.Helper;
+using Newtonsoft.Json;
+
+namespace KeagensBakery.Controllers
+{
+    public class UserController : Controller
+    {
+        HelperAPI helperApi = new HelperAPI();
+        string BaseURL = "https://localhost:44389/";
+        public async Task<IActionResult> GetUsers()
+        {
+
+            List<Users> user = new List<Users>();
+            HttpClient client = helperApi.client();
+            HttpResponseMessage res = await client.GetAsync("api/users");
+            if(res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                user = JsonConvert.DeserializeObject<List< Users >> (results);
+            }
+            return View(user);
+            
+        }
+    }
+}
---
+++ KeagenBakeryDemo/KeagensBakery/Logic/ApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace KeagensBakery.Logic
+{
+    public class ApiClient
+    {
+        //This class contains the logic for connecting 
+        //public HttpClient client = new HttpClient();
+
+      
+
+    }
+}
