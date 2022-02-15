e64cafecebf6b6e7e97602b6cd63e7aeb6e5f8ae
Brandon Toppel <toppelbman@gmail.com>
2022-02-15 16:53:54 -0500

    Added new menu Icon
    Added a new menu Icon to open and close the menu for the mobile version of the website.

--- keagens-bakery/src/components/Navbar.js
+++ keagens-bakery/src/components/Navbar.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <div className='Navbar'>
         <h1 id='logo'>Keagen's Bakery</h1>
-        <ul className='NavMenu'>
-            <li id='NavItem'>Home</li>
-            <li id='NavItem'>Bakery</li>
-            <li id='NavItem'>About</li>
-            <li id='NavItem'>Contact</li>
+        <button onClick={NavControl} className="fa-Bars"><FontAwesomeIcon icon = {faBars}  color="black" size='xl'></FontAwesomeIcon></button>
+        <div className='NavMenu'>
+        <ul id='NavItem'>
+            <li>Home</li>
+            <li>Bakery</li>
+            <li>About</li>
+            <li>Contact</li>
         </ul>
+        </div>
     </div>
   )
+}
+
+//function that will display the mobile navigation menu
+function NavControl() {
+  var x = document.getElementById("NavItem");
+
+  if(x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none";
+  }
 }
 
 export default Navbar
